0443a0d411126bbcf508bc4df382c27283dbc88d
bianzheng123 <1317257555@qq.com>
2020-09-15 14:46:15 +0800

    Update vecs_io.py

--- vecs_io.py
+++ vecs_io.py
@@ -1,11 +1,11 @@
 import numpy as np
 
-np.set_printoptions(threshold=np.inf)  # 打印numpy数组时显示全部的内容
+# 用于解析vecs后缀的文件
+# np.set_printoptions(threshold=np.inf)  # 打印numpy数组时显示全部的内容
 
 
 def ivecs_read(fname):
     a = np.fromfile(fname, dtype='int32')
-    print(a)
     d = a[0]
     return a.reshape(-1, d + 1)[:, 1:].copy()
 
@@ -19,4 +19,6 @@ def bvecs_read(fname):
     d = a[:4].view('uint8')[0]
     return a.reshape(-1, d + 4)[:, 4:].copy()
 
-# groundtruth_data = bvecs_read("bigann/bigann_query.bvecs")
+
+base_data = fvecs_read("siftsmall/siftsmall_base.fvecs")
+print(base_data)
